743f7e4efd25447f0400f17e0ad63f13d2aa6bb1
Daniel Santos <danielsantos@sysfar.com.br>
2023-06-21 02:12:04 -0300

    feat:Implemented product table and user seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => 'secret',
+            'remember_token' => 'teste'
+        ]);
+    }
+}
